f171f14cb2ef98b54c2ba047bf672809d428019d
Akash Mukherjee <akash3.11.2000@gmail.com>
2022-07-10 12:32:17 +0530

    Tailwind project Flora website added

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,4 +1,6 @@
-const checkOutMore = (e) => {
+const checkOutMore = async (e) => {
+  const audio = new Audio('./pokeball.mp3')
+  await audio.play();
   const page = "./trainers/" + e.target.value + ".html";
   if (confirm("Ready to go?")) {
     window.location.href = page;
