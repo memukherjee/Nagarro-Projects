ea14ec77fe6f6fc5710c78554ead26e27fd8d924
Akash Mukherjee <akash3.11.2000@gmail.com>
2022-07-12 07:16:15 +0530

    todoapp js logic code is made DRY

--- ToDoApp-jQuery/index.js
+++ ToDoApp-jQuery/index.js
@@ -1,14 +1,6 @@
 $(document).ready(function () {
-  $("#input").change(function () {
-    var input = $(this).val();
-    if (input !== "") {
-      $("ul").append(
-        "<li>" + input + '<i class="fas fa-check fa-trash "> </li>'
-      );
-      $(this).val("");
-    }
-  });
-  $("#add-btn").click(() => {
+
+  const addTask = ()=>{
     var input = $("#input").val();
     if (input !== "") {
       $("ul").append(
@@ -16,6 +8,13 @@ $(document).ready(function () {
       );
       $("#input").val("");
     }
+  }
+
+  $("#input").change(function () {
+    addTask();
+  });
+  $("#add-btn").click(() => {
+    addTask();
   });
   $("ul").on("click", ".fa-trash", function () {
     $(this).parent("li").fadeOut(200);
